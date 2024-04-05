e74f57ab0b38d20e5185274bc8adbd4827be1259
Adheesh <adheeshchat@gmail.com>
2024-04-05 15:08:21 -0700

    added map to store keyframes and landmarks

---
+++ include/map.h
@@ -0,0 +1,43 @@
+#pragma once
+#ifndef MAP_H
+#define MAP_H
+
+#include "commonInclude.h"
+#include "frame.h"
+#include "mapPoint.h"
+
+class Map {
+public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
+    typedef std::shared_ptr<Map> Ptr;
+    typedef std::unordered_map<unsigned long, MapPoint::Ptr> LandmarksType;
+    typedef std::unordered_map<unsigned long, Frame::Ptr> KeyFramesType;
+
+    Map(){};
+
+    void insertKeyFrame(Frame::Ptr frame);
+    void insertMapPoint(MapPoint::Ptr mapPoint);
+    LandmarksType getAllMapPoints();
+    KeyFramesType getAllKeyFrames();
+
+    LandmarksType getActiveMapPoints();
+    KeyFramesType getActiveKeyFrames();
+
+    void cleanMap();  // Remove points with zero observations in the map
+
+private:
+    void removeOldKeyFrame();
+
+    std::mutex _dataMutex;
+    LandmarksType _landmarks;
+    LandmarksType _activeLandmarks;
+    KeyFramesType _keyframes;
+    KeyFramesType _activeKeyFrames;
+
+    Frame::Ptr _currentFrame = nullptr;
+
+    //settings
+    int _numActiveKeyFrames = 7;
+};
+
+#endif  // MAP_H
---
+++ src/map.cpp
@@ -0,0 +1,112 @@
+#include "map.h"
+#include "feature.h"
+
+void Map::insertKeyFrame(Frame::Ptr frame) {
+    _currentFrame = frame;
+    if (_keyframes.find(frame->_keyframeID) == _keyframes.end()) {
+        _keyframes.insert(std::make_pair(frame->_keyframeID, frame));
+        _activeKeyFrames.insert(std::make_pair(frame->_keyframeID, frame));
+    } else {
+        _keyframes[frame->_keyframeID] = frame;
+        _activeKeyFrames[frame->_keyframeID] = frame;
+    }
+
+    if (_activeKeyFrames.size() > _numActiveKeyFrames)
+        removeOldKeyFrame();
+}
+
+void Map::insertMapPoint(MapPoint::Ptr mapPoint) {
+    if (_landmarks.find(mapPoint->_id) == _landmarks.end()) {
+        _landmarks.insert(std::make_pair(mapPoint->_id, mapPoint));
+        _activeLandmarks.insert(std::make_pair(mapPoint->_id, mapPoint));
+    } else {
+        _landmarks[mapPoint->_id] = mapPoint;
+        _activeLandmarks[mapPoint->_id] = mapPoint;
+    }
+}
+
+Map::LandmarksType Map::getAllMapPoints() {
+    std::unique_lock<std::mutex> _lck(_dataMutex);
+    return _landmarks;
+}
+
+Map::KeyFramesType Map::getAllKeyFrames() {
+    std::unique_lock<std::mutex> _lck(_dataMutex);
+    return _keyframes;
+}
+
+Map::LandmarksType Map::getActiveMapPoints() {
+    std::unique_lock<std::mutex> _lck(_dataMutex);
+    return _activeLandmarks;
+}
+
+Map::KeyFramesType Map::getActiveKeyFrames() {
+    std::unique_lock<std::mutex> _lck(_dataMutex);
+    return _activeKeyFrames;
+}
+
+void Map::removeOldKeyFrame() {
+    if (_currentFrame == nullptr)
+        return;
+
+    double maxDist = 0, minDist = 9999;
+    double maxKfID = 0, minKfID = 0;
+    auto TWC = _currentFrame->getPose().inverse();
+
+    for (auto& kf : _activeKeyFrames) {
+        if (kf.second == _currentFrame)
+            continue;
+        auto dist = (kf.second->getPose() * TWC).log().norm();
+
+        if (dist > maxDist) {
+            maxDist = dist;
+            maxKfID = kf.first;
+        }
+        if (dist < minDist) {
+            minDist = dist;
+            minKfID = kf.first;
+        }
+    }
+
+    const double minDistThreshold = 0.2;
+    Frame::Ptr frameToRemove = nullptr;
+    if (minDist < minDistThreshold)  // remove the most recent frames (within threshold), otherwise remove farthest
+        frameToRemove = _keyframes.at(minKfID);
+    else
+        frameToRemove = _keyframes.at(maxKfID);
+
+    LOG(INFO) << "removed keyframe: " << frameToRemove->_keyframeID;
+    _activeKeyFrames.erase(frameToRemove->_keyframeID);
+    for (auto feat : frameToRemove->featuresLeft) {
+        if (feat == nullptr)
+            continue;
+        auto mp = feat->_mapPoint.lock();
+        if (mp) {
+            mp->removeObservations(feat);
+        }
+    }
+    for (auto feat : frameToRemove->featuresRight) {
+        if (feat == nullptr)
+            continue;
+        auto mp = feat->_mapPoint.lock();
+        if (mp) {
+            mp->removeObservations(feat);
+        }
+    }
+
+    cleanMap();
+}
+
+void Map::cleanMap() {
+    int landmarksRemovedCount = 0;
+    for (auto iter = _activeLandmarks.begin(); iter != _activeLandmarks.end();) {
+        if (iter->second->observedTimes == 0) {
+            iter = _activeLandmarks.erase(iter);
+            landmarksRemovedCount++;
+        } else {
+            iter++;
+        }
+    }
+
+    LOG(INFO) << "Removed landmarks: " << landmarksRemovedCount;
+}
